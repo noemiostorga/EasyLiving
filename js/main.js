177d27a55112bd3d746cce6c95329391394af5e3
noemiostorga <noemiostorga@gmail.com>
2015-12-08 12:16:54 -0800

    added upload image

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*var upload = function(){
+  document.getElementById("upload_widget_opener").addEventListener("click", function() {
+
+    cloudinary.openUploadWidget({ cloud_name: 'demo', upload_preset: 'a5vxnzbp'}, 
+      function(error, result) { console.log(error, result) });
+
+  }, false);
+
+};*/
+
+function myFunction() {
+    var x = document.getElementById("myFile");
+    x.disabled = true;
+}
+
