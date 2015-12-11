9dfd652476725b76b3c5285eb6a3c9478e94d0b4
noemiostorga <noemiostorga@gmail.com>
2015-12-11 13:33:26 -0800

    backbone

--- js/main.js
+++ js/main.js
@@ -1,8 +1,63 @@
-// console.log('test');
-// $.ajax({
-// 	url:'https://api.cloudinary.com/v1_1/easyliving',
-// 	method: 'POST',
-// 	dataType: 'json'
-// }).then(function(resp){
-// 	console.log(resp);
-// });
+  console.log('test');
+
+$(document).ready(function(){
+
+		 $('#upload_widget_opener').cloudinary_upload_widget(
+			{ cloud_name: 'easyliving', upload_preset: 'n4ejdzb7', 
+			 cropping: 'server', 'folder': 'user_photos' },
+			function(error, result) { console.log(error, result) });
+
+
+	var Images = Backbone.Model.extend({
+
+
+		initialize: function(){
+			$(document).oAn('cloudinarywidgetsuccess', function(e, data) {
+		  console.log("Global", e, data); });
+		}
+
+});
+	
+	
+});
+
+//instagram 
+//fetch set save
+		
+		/*defaults:{
+				url: null,
+				path: null,
+				etag: null,
+				secure_url: null,
+				signature: null,
+				thumbnail: null
+			},
+
+		validate: function(){
+			if(!attrs.url){
+				return "url is required";
+			}
+			if (!attrs.path){
+				return "path is required";
+			}
+			if (!attrs.etag){
+				return "etag is required";
+			}
+			if (!attrs.secure_url){
+				return "secure_url is required";
+			}
+			if (!attrs.signature){
+				return "signature is required";
+			}
+			if (!attrs.thumbnail){
+				return "thumbnail is required";
+			}
+
+		},
+		cloud_name: "easyliving"
+
+		});
+*/ 
+
+
+
--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-console.log('test');
-$.ajax.({
-	url:'https://api.cloudinary.com/v1_1/easyliving',
-	method: 'POST',
-	dataType: 'json'
-}).then(function(resp){
-	console.log(resp);
-});
+
 
 // var RoomImages = Backbone.Model.extend({
 // 	initialize: function(){
