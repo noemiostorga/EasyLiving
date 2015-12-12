478ffa76d40f959a25755e6cedba8d37a2f25793
noemiostorga <noemiostorga@gmail.com>
2015-12-12 12:55:05 -0800

    google script

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
   console.log('test');
 
-$(document).ready(function(){
+
 
 		 $('#upload_widget_opener').cloudinary_upload_widget(
 			{ cloud_name: 'easyliving', upload_preset: 'n4ejdzb7', 
@@ -8,61 +8,42 @@ $(document).ready(function(){
 			function(error, result) { console.log(error, result) });
 
 
-	var Images = Backbone.Model.extend({
 
 
-		initialize: function(){
-			$(document).on;('cloudinarywidgetsuccess', function(e, data) {
-		  console.log("Global", e, data); });
-		}
-	});
-	
-	
+$(document).on('cloudinarywidgetsuccess', function(e, data) {
+
+		console.log("Global", e, data); 
+			var Images = Backbone.Model.extend({
+				Model: Images,
+				url: "https://www.parse.com/apps/easyliving/collections#class/Rooms"
+		});
+
+		var ImagesCollection = new Images();
+		ImagesCollection.fetch({
+			success: function(resp){
+				var Imagedata = {"path": resp.toJSON()};
+				var PhotoTemplate=$("#photoTemplate").text();
+				var data = Mustache.render(PhotoTemplate, Imagedata);
+				$("#photos").html(data);
+			},
+			error: function(err){
+				console.log("error:", err);
+				}
+			});
+
 });
+
+	
+
+
+
+
+
+	
+
+
 
 //instagram 
 //fetch set save
 		
-		defaults:{
-				url: null,
-				path: null,
-				etag: null,
-				secure_url: null,
-				signature: null,
-				thumbnail: null
-			},
-
-		validate: function(){
-			if(!attrs.url){
-				return "url is required";
-			}
-			if (!attrs.path){
-				return "path is required";
-			}
-			if (!attrs.etag){
-				return "etag is required";
-			}
-			if (!attrs.secure_url){
-				return "secure_url is required";
-			}
-			if (!attrs.signature){
-				return "signature is required";
-			}
-			if (!attrs.thumbnail){
-				return "thumbnail is required";
-			}
-
-		},
-		cloud_name: "easyliving"
-
-		});
-
-	var image = Backbone.Collection.extend({
-		model: Images,
-		_cloud_name: easyliving
-
-	});	
-
-
-
-
+		
