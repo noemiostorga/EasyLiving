8c6cc26d2be10b0fe8fc3b8cc63014fef7b1f083
noemiostorga <noemiostorga@gmail.com>
2015-12-12 21:47:48 -0800

    parse won't save

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
   console.log('test');
 
 
-
 	$('#upload_widget_opener').cloudinary_upload_widget(
 			{ cloud_name: 'easyliving', upload_preset: 'n4ejdzb7', 
 			 cropping: 'server', 'folder': 'user_photos' },
@@ -12,39 +11,104 @@
 	$(document).on('cloudinarywidgetsuccess', function(e, data) {
 
 		console.log("Global", e, data); 
+		console.log(data);
+		picture_url = data[0].url
+		console.log(picture_url);
 		
-		});
-
-
-
-
-	var Images = Backbone.Model.extend({
-		Default: function(){
-			url: null	
-		}
 	});
 
-	var ImageCollection= Backbone.Collections.extend({
-			Model: Images,
-			url: "https://www.parse.com/apps/easyliving/collections#class/Rooms"
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+	var Image = Backbone.Model.extend({
+		url: "https://www.parse.com/apps/easy--37/collections#class/Room",
+		initialize: function(){
+		
+
+		},defaults: {
+			image_url: null,
+			url: null
+		},
+		_parse_class_name: "Image"
 	});
 
-	var imageCollection = new ImagesCollection();
-		imagesCollection.fetch({
-			success: function(resp){
-					var Imagedata = {"path": resp.toJSON()};
-					var PhotoTemplate=$("#photoTemplate").text();
-					var data = Mustache.render(PhotoTemplate, Imagedata);
-					$("#photos").html(data);
-			},
-			error: function(err){
-					console.log("error:", err);
-			}
+	var ImageCollection= Backbone.Collection.extend({
+			model: Image,
+			_parse_class_name: "Image"
+	});
 
-		});
 
+
+	var imageCollection = new ImageCollection();
+
+	var image = new Image();
+
+			image.set({
+				url: 'https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg'
+			})
+			image.save(null, {
+				success: function(resp){
+				},error: function(err){
+					console.log("error", err);
+				}
+			});
+
+
+
+
+
+	// 	imageCollection.set({
+
+	// 		success: function(resp){
+	// 			console.log("JSON data: ", resp.toJSON());
+	// 				var PhotoTemplate=$("#photoTemplate").text();
+	// 				var data = Mustache.render(PhotoTemplate, Imagedata);
+	// 				$("#photos").html(data);
+	// 		},
+	// 		error: function(err){
+	// 				console.log("error:", err);
+	// 		}
+
+	// 	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*var ImageCollection = Backbone.View.extend({
+
+
+		 var data = JSON.stringify($('form').serializeObject());
+            this.model.set(data);
+	})
 
 /*
 
