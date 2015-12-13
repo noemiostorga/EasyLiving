d326cb46c6f314b82a324753438eefc9e47cd417
noemiostorga <noemiostorga@gmail.com>
2015-12-12 16:58:39 -0800

    uncommented cloudinary models

--- js/main.js
+++ js/main.js
@@ -5,32 +5,54 @@
 	$('#upload_widget_opener').cloudinary_upload_widget(
 			{ cloud_name: 'easyliving', upload_preset: 'n4ejdzb7', 
 			 cropping: 'server', 'folder': 'user_photos' },
+
 			function(error, result) { console.log(error, result) });
+
 
 	$(document).on('cloudinarywidgetsuccess', function(e, data) {
 
 		console.log("Global", e, data); 
-			var Images = Backbone.Model.extend({
-				Model: Images,
-				url: "https://www.parse.com/apps/easyliving/collections#class/Rooms"
+		
 		});
 
-		var ImagesCollection = new Images();
-		ImagesCollection.fetch({
-			success: function(resp){
-				var Imagedata = {"path": resp.toJSON()};
-				var PhotoTemplate=$("#photoTemplate").text();
-				var data = Mustache.render(PhotoTemplate, Imagedata);
-				$("#photos").html(data);
-			},
-			error: function(err){
-				console.log("error:", err);
-				}
-			});
+
+
+
+	var Images = Backbone.Model.extend({
+		Default: function(){
+			url: null	
+		}
 	});
 
+	var ImageCollection= Backbone.Collections.extend({
+			Model: Images,
+			url: "https://www.parse.com/apps/easyliving/collections#class/Rooms"
 
-/*$(document).on('map', function(data){
+
+	});
+
+	var imageCollection = new ImagesCollection();
+		imagesCollection.fetch({
+			success: function(resp){
+					var Imagedata = {"path": resp.toJSON()};
+					var PhotoTemplate=$("#photoTemplate").text();
+					var data = Mustache.render(PhotoTemplate, Imagedata);
+					$("#photos").html(data);
+			},
+			error: function(err){
+					console.log("error:", err);
+			}
+
+		});
+
+
+/*
+
+var Images = Backbone.Model.extend({
+				Model: Images,
+				url: "https://www.parse.com/apps/easyliving/collections#class/Rooms"
+
+$(document).on('map', function(data){
 	function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
