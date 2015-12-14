f96247a3ebf22b59ca57080e0a7ccf120bc29e91
noemiostorga <noemiostorga@gmail.com>
2015-12-13 21:11:11 -0800

    added googles map snippet

--- js/google.js
+++ js/google.js
@@ -23,3 +23,14 @@ var Place=Backbone.View.extend({
 	}
 
 });
+
+	
+$(document).ready(function(){
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(success);
+	} else {
+	  error('Geo Location is not supported');
+	}
+	console.log("fd");
+
+});
