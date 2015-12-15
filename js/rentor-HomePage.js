3912610ce69ab318c84c3a44ab857f669624ea9b
noemiostorga <noemiostorga@gmail.com>
2015-12-15 15:43:04 -0800

    implying links to home

---
+++ js/rentor-HomePage.js
@@ -0,0 +1,71 @@
+
+
+var Rooms = Backbone.Model.extend({
+	initialize: function(){
+
+	}, defualts: {
+		image_url: null
+	},
+		_parse_class_name: "Room"
+
+})
+
+var Room = Backbone.Collection.extend({
+	model: Rooms,
+	_parse_class_name: "Room"
+});
+
+var ImageCollection = new Room();
+
+ImageCollection.fetch({
+	success: function(resp){
+
+	},
+	error: function(err){
+		console.log(err);
+	}
+});
+
+
+/////////////////////
+
+var Renter = Backbone.Router.extend({
+	initialize: function(){
+
+		Backbone.history.start({pushState: true});
+	},
+		routes: {
+			"" : "renter_view"
+		}
+
+	help: function(){
+		console.log("Hfe");
+
+	},
+	search: function(){
+		console.log("searching");
+	},
+
+});
+
+
+
+
+var router = new Router();
+	router.on('')
+
+
+
+/*var SignupRentee= Backbone.Model.extend({
+	document.getElementById("Signup-firstname").val();
+	document.getElementById("signup-lastname".val();
+	document.getElementById("signup-email").val();
+	document.getElementById("signup-passsword").val();
+	document.getElementById("signup-confirm").val();	
+})
+
+*/
+var renter_view = new Renter({el: $("#view_container")});
+
+
+
