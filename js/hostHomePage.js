f88a90384cf8dfe543601fe74d348bf8c75187c6
Monica <monicamillerg@gmail.com>
2015-12-16 00:03:48 -0800

    can add a room now

--- js/hostHomePage.js
+++ js/hostHomePage.js
@@ -15,7 +15,8 @@ var AppView = Backbone.View.extend({
   app: function(e) {
     var self  = this; 
     
-    
+    new CreteListingView();
+    return false;
     
   },
 
@@ -29,68 +30,115 @@ var AppView = Backbone.View.extend({
 
 var CreteListingView = Backbone.View.extend({
   events: {
-    "submit form.create-listing-form": "app",
+    "submit form.create-listing-form": "createListing",
   },
 
   el: ".content",
   
   initialize: function() {
-    _.bindAll(this, "app");
+    _.bindAll(this, "createListing");
     this.render();
   },
 
-  app: function(e) {
-  //
-   // Capture form input
-      var squareFoot = $('input[name=squareFoot]:checked').val();
-      var bathroom = $('input[name=bathroom]:checked').val();
-      var smoke = $('input[name=smoke]:checked').val();
-      var furniture = $('input[name=furniture]:checked').val();
-      var kids = $('input[name=kidss]:checked').val();
-      var pets = $('input[name=pets]:checked').val();
+  createListing: function(e) {
 
+    // Capture form input
+    var self = this;
+
+    var title = $('input[name=title]').val();
+    var address = $('input[name=address]').val();
+    var city = $('input[name=city]').val();
+    var state = $('input[name=state]').val();
+    var zipCode = $('input[name=zipCode]').val();
+    var price = parseInt($('input[name=price]').val());
+    var sqft = $('input[name=sqft]').val();
+    var description = $('textarea[name=description]').val();
+    var privateBathroom = $('input[name=privateBathroom]').is(':checked');
+    var furnished = $('input[name=furnished]').is(':checked');
+    var noSmoking = $('input[name=noSmoking]').is(':checked');
+    var noKids = $('input[name=noKids]').is(':checked');
+    var noPets = $('input[name=noPets]').is(':checked');
 
       // Instantiate the object we're going to create in Parse
-      var CreteListingView = Parse.Object.extend("Room");
-      var CreteListingView = new CreteListingView();
+    var Room = Parse.Object.extend("Room");
+    var room = new Room();
 
       // Save the object instance to Parse
-      CreteListingView.save({
-        squareFoot: squareFoot,
-        bathroom: bathroom,
-        smoke: smoke,
-        furniture: furniture,
-        kids:kids,
-        pets: pets,
+      room.save({
+        title: title,
+        address: address,
+        city: city,
+        state: state,
+        zipCode: zipCode,
+        price: price,
+        sqft: sqft,
+        description: description,
+        privateBathroom: privateBathroom,
+        furnished: furnished,
+        noSmoking: noSmoking,
+        noKids: noKids,
+        noPets: noPets,
         user: Parse.User.current()     
       }).then(function(result) {
-
-        // on success, move to the next page (dashboard)
-        window.location = "host.homePage.html"; 
+        new AppView();
       }, function(error) {
-        // on failure, you should do something? Maybe? Dunno.
-        console.log("check your infomation")
+          self.$(".create-listing-form .error").html(_.escape(error.message)).show();
+          self.$(".create-listing-form button").removeAttr("disabled");
+
       });
+
+      this.$(".create-listing-form button").attr("disabled", "disabled");
     },
 
     // This renders the view
     render: function() {
-      var squareFoot = ["9' x 9'","13' x 13'","15' x 15"];
-      var bathroom = ['private', 'share'];
-      var smoke = ["yes", "no"];
-      var furniture = ["yes", "no"];
-      var pets = ["yes", "no"];
+      var data = {
+        fields: [
+          {name: "title",
+            description: "Listing Title",
+            type: "text"},
+          {name: "address",
+            description: "Address",
+            type: "text"},
+          {name: "city",
+            description: "City",
+            type: "text"},
+          {name: "state",
+            description: "State",
+            type: "text"},
+          {name: "zipCode",
+            description: "Zipcode",
+            type: "text"},
+          {name: "price",
+            description: "Price",
+            type: "text"},
+          {name: "sqft",
+            description: "Sqft",
+            type: "text"},
+        ],
+        amenities: [
+          { description: "Private Bathroom",
+            value: "privateBathroom"},
+          { description: "Furnished",
+            value: "furnished"},
+        ],
+        restrictions: [
+          { description: "No smoking",
+            value: "noSmoking"
+          },
+          { description: "No kids",
+            value: "noKids"
+          },
+          { description: "No pets",
+            value: "noPets"
+          }
+        ]
+
+      }
 
 
-      var listingTemplate = $("#create-listing--template").text();
-      var templateHtml = Mustache.render(listingTemplate, {
-        squareFoot: squareFoot,
-        bathroom: bathroom,
-        smoke: smoke,
-        furniture: furniture,
-        kids:kids,
-        pets: pets
-      });
+      var listingTemplate = $("#create-listing-template").text();
+      var templateHtml = Mustache.render(listingTemplate, data);
       this.$el.html(templateHtml);
       this.delegateEvents();
     }
