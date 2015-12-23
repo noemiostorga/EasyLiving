6cdf70d1fa4c288eff5abaca799a89fc4889cf5e
noemiostorga <noemiostorga@gmail.com>
2015-12-22 16:22:31 -0800

    added reset button

--- js/rentor-HomePage.js
+++ js/rentor-HomePage.js
@@ -1,12 +1,11 @@
+(function(){
+
+	Parse.$ = jQuery;
+
+	Parse.initialize("8FursUpxatbb97bkZXQo0UOhEveyAvcOU2UNs7ZV", "nCv32lT0DhjotclrySOuLi6iAR4LCyrKE3y130Xg");
 
 
-Parse.initialize("8FursUpxatbb97bkZXQo0UOhEveyAvcOU2UNs7ZV", "nCv32lT0DhjotclrySOuLi6iAR4LCyrKE3y130Xg");
-
-
-
-
-
-var RenterView = Backbone.View.extend({
+	var RenterView = Backbone.View.extend({
 
 	events: {
 		"submit form.userprofile-form": "createRenterView",
@@ -18,14 +17,40 @@ var RenterView = Backbone.View.extend({
 		_.bindAll(this, "createRenterView");
 		this.render();
 	},
-	createRenterView: function(){
-		/*$("#createRenterView")
-
+	createRenterView: function(e){
+		var Renter = this;
 		//use mustache here
+		var RoomImages= $("#RoomImages").val();
+		var roomImages= $("#roomImages").val();
 
-		var RoomImages = Parse.Object.extend("RoomImages");
-		var roomImages = new RoomImages();
 
+
+		var Rooms = Parse.Object.extend("Rooms");
+		var rooms = new Rooms();
+
+		room.save({
+
+
+
+		}).then(function(result){
+			new AppView();
+		}, function(error){
+			renter.$(".userprofile-form .error").html(_.escape(error.message)).show();
+			renter.$(".userprofile-form button").removeAttr("disabled");
+		});
+
+		this.$(".userprofile-form button").attr("disabled", "disabled");
+
+		return false;
+
+	},
+
+	render: function(){
+		var 
+	}
+
+
+///////////////
 		roomImages.set("url",  "http://www.arrowwoodresort.com/up/gallery/exec_family_room.jpg");
 		roomImages.save(null, {
 			success: function(roomImages){
@@ -156,4 +181,4 @@ var renter_view = new Renter({el: $("#view_container")});
 
 
 
-}
+})();
