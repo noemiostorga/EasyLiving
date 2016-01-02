17b21132ec438a905a2f904f3226cd481097d017
noemiostorga <noemiostorga@gmail.com>
2016-01-01 22:20:09 -0800

    need to figure out how to install other markers

--- js/google.js
+++ js/google.js
@@ -6,11 +6,11 @@ function initMap() {
     zoom:11
   });
 
-  var myLatLng = {lat: 36.1406013, lng: -115.3161044};
+  var houses ={lat: 36.1406013, lng: -115.3161044};
 
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
+    center: houses,
     scrollwheel: false,
     zoom: 11
   });
@@ -18,7 +18,7 @@ function initMap() {
   // Create a marker and set its position.
   var marker = new google.maps.Marker({
     map: map,
-    position: myLatLng,
+    position: houses,
     title: 'Hello World!'
   });
 }
