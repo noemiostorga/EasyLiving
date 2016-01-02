9569ed3e206da79714dcef6971ae255302651009
noemiostorga <noemiostorga@gmail.com>
2016-01-01 21:29:58 -0800

    google map marker on house

--- js/google.js
+++ js/google.js
@@ -5,5 +5,21 @@ function initMap() {
     scrollwheel: false,
     zoom:11
   });
+
+  var myLatLng = {lat: 36.1406013, lng: -115.3161044};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 11
+  });
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: 'Hello World!'
+  });
 }
  
