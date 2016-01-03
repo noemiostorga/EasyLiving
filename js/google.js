6364608de583b9bec9873742a232d3a36ecebab8
noemiostorga <noemiostorga@gmail.com>
2016-01-02 20:50:40 -0800

    added banner on google maps window

--- js/google.js
+++ js/google.js
@@ -5,31 +5,31 @@
       
         "lat": '36.1146832',
         "lng": '-115.2555595',
-        "description": 'personality type? '
+        "description": '<div class="MapDescription">Work: 25 minutes <br> commonPlace: 40 minutes <br> personality: introvert</div>'
     },
     {
        
         "lat": '36.1252147',
         "lng": '-115.291785',
-        "description": 'this is not his crib fyi.'
+        "description": '<div class="MapDescription">Work: 17 minutes <br> commonPlace: 30 minutes <br> personality: outgoing</div>'
     },
     {
     
         "lat": '36.1448303',
         "lng": '-115.302484',
-        "description": 'dummy lat and lng.'
+        "description": '<div class="MapDescription">Work: 54 minutes <br> commonPlace: 12 minutes <br> personality: amnivert</div>'
     },
     {
         
         "lat": '36.1446858',
         "lng": '-115.1753368',
-        "description": 'testing 4.'
+        "description": '<div class="MapDescription">Work: 45 minutes <br> commonPlace: 20 minutes <br> personality: nightowl</div>'
     },
     {
         
         "lat": '36.0338694',
         "lng": '-115.2676475',
-        "description": 'empty space.'
+        "description": '<div class="MapDescription">Work: 36 minutes <br> commonPlace: 23 minutes <br> personality: earlybird</div>'
     }
     ];
     window.onload = function () {
@@ -38,7 +38,7 @@
     function LoadMap() {
         var mapOptions = {
             center: new google.maps.LatLng(markers[0].lat, markers[0].lng),
-            zoom: 11,
+            zoom: 10,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -58,7 +58,8 @@
             (function (marker, data) {
                 google.maps.event.addListener(marker, "click", function (e) {
 
-                    infoWindow.setContent("<a href='detailRoom.html'>" + "<div class='housePre'>" + data.description + "</div>" + '</a>');
+                    infoWindow.setContent(
+                      "<a class='googleDescription' href='detailRoom.html'>" + "<div class='housePre'>" + data.description + "</div>" +  "</a>");
                     infoWindow.open(map, marker);
                 });
             })(marker, data);
@@ -95,7 +96,11 @@
   });
   drawingManager.setMap(map);
 
+  var work = {lat: 36.1583875, lng: -115.1546736};
+  var commonPlace= {lat:36.1252147 , lng:-115.291785 };
+  var home = {lat:36.0338694, lng:-115.2676475};
 
+  
 
   }//end of init map function
 
